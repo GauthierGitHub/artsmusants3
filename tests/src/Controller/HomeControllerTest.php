dd426bd72e415b5e3c0f26f71977ea9802e36f0e
Gauthier <gauthier.emailpro@gmail.com>
2019-05-04 20:42:45 +0200

    HomeControllerTest

--- tests/src/Controller/HomeControllerTest.php
+++ tests/src/Controller/HomeControllerTest.php
@@ -3,7 +3,7 @@
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
-use App\Kernel;// https://stackoverflow.com/questions/49745500/how-to-unit-test-symfony-controllers
+use App\Kernel; // https://stackoverflow.com/questions/49745500/how-to-unit-test-symfony-controllers
 use App\Controller\HomeController;
 
 class HomeControllerTest extends TestCase
